b57360bc1c9d598d6eed4b39e2953dd9641fe255
qbisi <qbisicwate@gmail.com>
2025-01-31 09:47:18 +0800

    flake: nixpkgs update to 24.11

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     # nixpkgs.url = "/home/qbisi/nixpkgs";
     nixos-images = {
       url = "github:qbisi/nixos-images";
@@ -12,7 +12,7 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
