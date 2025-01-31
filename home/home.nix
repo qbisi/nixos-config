b1d26ec34a9d7e90fb28928f7bcd9c78104eb388
qbisi <qbisicwate@gmail.com>
2025-01-31 09:37:18 +0800

    home: add tmux

--- home/home.nix
+++ home/home.nix
@@ -37,6 +37,33 @@
       enableBashIntegration = true;
       nix-direnv.enable = true;
     };
+    tmux = {
+      shortcut = "a";
+      # aggressiveResize = true; -- Disabled to be iTerm-friendly
+      baseIndex = 1;
+      newSession = true;
+      # Stop tmux+escape craziness.
+      # escapeTime = 0;
+      # Force tmux to use /tmp for sockets (WSL2 compat)
+      secureSocket = false;
+      enable = true;
+      plugins = with pkgs; [
+        tmuxPlugins.better-mouse-mode
+      ];
+
+      extraConfig = ''
+        set -g status off
+        set -g set-titles on
+        set -g set-titles-string "#{USER}@#H:#{PWD}"
+
+        # Mouse works as expected
+        set-option -g mouse on
+        # easy-to-remember split pane commands
+        bind | split-window -h -c "#{pane_current_path}"
+        bind - split-window -v -c "#{pane_current_path}"
+        bind c new-window -c "#{pane_current_path}"
+      '';
+    };
   };
 
   services.vscode-server = {
