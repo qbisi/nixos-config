d284ffee7fe69574595d0ccc4230c5687f75d0e4
qbisi <qbisicwate@gmail.com>
2025-02-16 23:20:57 +0800

    hm: use master branch

--- flake.nix
+++ flake.nix
@@ -14,7 +14,8 @@
       inputs.nixpkgs-lib.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.11";
+      # url = "github:nix-community/home-manager/release-24.11";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
