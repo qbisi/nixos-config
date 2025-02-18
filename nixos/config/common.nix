b4c42d99bd75079e861cb540974f55fb99097075
qbisi <qbisicwate@gmail.com>
2025-02-19 02:30:01 +0800

    registry add agenix and colmena

--- nixos/config/common.nix
+++ nixos/config/common.nix
@@ -144,6 +144,19 @@
   };
 
   nix = {
+    registry = {
+      agenix.to = {
+        owner = "ryantm";
+        repo = "agenix";
+        type = "github";
+      };
+      colmena.to = {
+        owner = "zhaofengli";
+        repo = "colmena";
+        type = "github";
+      };
+    };
+
     settings = {
       experimental-features = [
         "nix-command"
@@ -156,10 +169,12 @@
       substituters = [
         # "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://nix-community.cachix.org"
+        "https://colmena.cachix.org"
       ];
 
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       ];
     };
 
