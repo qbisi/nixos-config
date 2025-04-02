6a5e1216f2eeaa803750fe8d10278b612752ad4f
qbisi <qbisicwate@gmail.com>
2025-04-02 15:21:11 +0800

    fix wireguard module

--- config/common/wireguard.nix
+++ config/common/wireguard.nix
@@ -7,30 +7,32 @@
   ...
 }:
 {
-  networking = lib.mkIf config.networking.wireguard.enable {
-    firewall.allowedUDPPorts = [ 51820 ];
-    wireguard.interfaces = {
-      wg0 = {
-        ips = [ "${self.vars.hosts."${config.networking.hostName}".wgip}/32" ];
-        listenPort = 51820;
-        privateKeyFile = config.age.secrets."wg-${config.networking.hostName}".path;
-        peers =
-          let
-            wgHosts = lib.filterAttrs (_: v: builtins.hasAttr "wgpub" v) self.vars.hosts;
-          in
-          lib.mapAttrsToList (
-            n: v:
-            {
-              name = n;
-              publicKey = v.wgpub;
-              allowedIPs = [ "${v.wgip}/32" ];
-            }
-            // (lib.optionalAttrs (!(self.lib.isPrivateIP v.ip)) {
-              endpoint = "${v.ip}:51820";
-              persistentKeepalive = 25;
-            })
-          ) wgHosts;
+  networking =
+    lib.mkIf (builtins.hasAttr "wgip" self.vars.hosts."${config.networking.hostName}" or { })
+      {
+        firewall.allowedUDPPorts = [ 51820 ];
+        wireguard.interfaces = {
+          wg0 = {
+            ips = [ "${self.vars.hosts."${config.networking.hostName}".wgip}/32" ];
+            listenPort = 51820;
+            privateKeyFile = config.age.secrets."wg-${config.networking.hostName}".path;
+            peers =
+              let
+                wgHosts = lib.filterAttrs (_: v: builtins.hasAttr "wgpub" v) self.vars.hosts;
+              in
+              lib.mapAttrsToList (
+                n: v:
+                {
+                  name = n;
+                  publicKey = v.wgpub;
+                  allowedIPs = [ "${v.wgip}/32" ];
+                }
+                // (lib.optionalAttrs (!(self.lib.isPrivateIP v.ip)) {
+                  endpoint = "${v.ip}:51820";
+                  persistentKeepalive = 25;
+                })
+              ) wgHosts;
+          };
+        };
       };
-    };
-  };
 }
