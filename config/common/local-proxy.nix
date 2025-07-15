843b0afddd0d1e91b9ceb02d28cd685d9cb5ba95
qbisi <qbisicwate@gmail.com>
2025-07-15 14:02:04 +0800

    local proxy: switch to ody

--- config/common/local-proxy.nix
+++ config/common/local-proxy.nix
@@ -15,6 +15,6 @@ let
 in
 {
   networking.proxy.default = lib.mkIf (builtins.elem config.networking.hostName hostnames) (
-    lib.mkDefault "http://${self.vars.hosts.h88k.ip}:1080"
+    lib.mkDefault "http://${self.vars.hosts.ody.ip}:1080"
   );
 }
