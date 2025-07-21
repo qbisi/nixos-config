78b95613c2c5646a1bf4379c62eed45949539093
qbisi <qbisicwate@gmail.com>
2025-07-21 18:21:28 +0800

    h88k: ssh via socks proxy

--- home/profiles/qbisi@h88k.nix
+++ home/profiles/qbisi@h88k.nix
@@ -19,5 +19,9 @@
 
   programs.ssh.matchBlocks."github.com".proxyJump = "sl2";
 
+  programs.ssh.matchBlocks."ody".proxyCommand = "nc -x 127.0.0.1:1080 -X 5 %h %p";
+  programs.ssh.matchBlocks."x79".proxyCommand = "nc -x 127.0.0.1:1080 -X 5 %h %p";
+  programs.ssh.matchBlocks."ft".proxyCommand = "nc -x 127.0.0.1:1080 -X 5 %h %p";
+
   home.packages = with pkgs; [ ];
 }
