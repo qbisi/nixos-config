e62669ef6368fdf4dc76bed211db9f9b3df0cf76
qbisi <qbisicwate@gmail.com>
2025-12-16 10:32:30 +0800

    update inputs nixos-images

--- flake.nix
+++ flake.nix
@@ -11,10 +11,6 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     colmena = {
       url = "github:zhaofengli/colmena";
       inputs.nixpkgs.follows = "nixpkgs";
