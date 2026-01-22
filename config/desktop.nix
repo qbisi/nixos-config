825a22de66e6d46372433e191ac1db28b3eb5c4e
qbisi <qbisicwate@gmail.com>
2026-01-23 05:23:50 +0800

    set font via hm

--- config/desktop.nix
+++ config/desktop.nix
@@ -25,28 +25,6 @@
 
   programs.system-config-printer.enable = true;
 
-  fonts.packages = with pkgs; [
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    noto-fonts-color-emoji
-    nerd-fonts.hack
-  ];
-
-  fonts.fontconfig = {
-    defaultFonts = {
-      emoji = [ "Noto Color Emoji" ];
-      monospace = [
-        "Hack Nerd Font"
-      ];
-      sansSerif = [
-        "Noto Sans CJK SC"
-      ];
-      serif = [
-        "Noto Serif CJK SC"
-      ];
-    };
-  };
-
   i18n = {
     defaultLocale = "en_US.UTF-8";
     inputMethod = {
--- home/home.nix
+++ home/home.nix
@@ -81,7 +81,27 @@
     patchelf
     ssh-to-age
     gcc
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-color-emoji
+    nerd-fonts.hack
   ];
+
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      emoji = [ "Noto Color Emoji" ];
+      monospace = [
+        "Hack Nerd Font"
+      ];
+      sansSerif = [
+        "Noto Sans CJK SC"
+      ];
+      serif = [
+        "Noto Serif CJK SC"
+      ];
+    };
+  };
 
   programs = {
     yazi = {
