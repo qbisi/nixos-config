5fcfe800a17014b0814694244fc452017b167264
qbisi <qbisicwate@gmail.com>
2026-02-05 07:33:20 +0800

    qbisi@mac: add some packages

--- home/profiles/qbisi@mac.nix
+++ home/profiles/qbisi@mac.nix
@@ -13,4 +13,18 @@
     http_proxy = "http://127.0.0.1:7897";
     https_proxy = http_proxy;
   };
+
+  home.packages = with pkgs; [
+    iproute2mac
+    htop
+    typst
+    (python3.withPackages (
+      ps: with ps; [
+        numpy
+        scipy
+        matplotlib
+        pytest
+      ]
+    ))
+  ];
 }
