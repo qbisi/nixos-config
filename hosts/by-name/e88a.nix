f65ed5bcfd56457535b92ed29d5465c967ef937f
qbisi <qbisicwate@gmail.com>
2026-03-22 22:16:38 +0800

    e88a: unset ALSA_UCM_CONF

--- hosts/by-name/e88a.nix
+++ hosts/by-name/e88a.nix
@@ -238,7 +238,7 @@
 
   environment.variables = {
     MESA_GLSL_VERSION_OVERRIDE = 330;
-    ALSA_CONFIG_UCM2 = "${pkgs.alsa-ucm-conf-rk3588}/share/alsa/ucm2";
+    # ALSA_CONFIG_UCM2 = "${pkgs.alsa-ucm-conf-rk3588}/share/alsa/ucm2";
   };
 
   environment.systemPackages = with pkgs; [
